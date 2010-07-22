d020c40ff86e37b5cb2adec2f71fafd1b4a0e18e
David Padilla <david.padilla@crowdint.com>
2010-07-22 00:54:38 -0500

    Renamed the Product class on the tests

--- spec/acts_as_shopping_cart_spec.rb
+++ spec/acts_as_shopping_cart_spec.rb
@@ -8,7 +8,7 @@ class SomeCartItem < ActiveRecord::Base
   acts_as_shopping_cart_item 'SomeCart'
 end
 
-class Product < ActiveRecord::Base
+class SomeClass < ActiveRecord::Base
 
 end
 
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(:version => 1) do
 
   end
 
-  create_table :products do |t|
+  create_table :some_classes do |t|
 
   end
 
@@ -44,18 +44,18 @@ describe "ActsAsShoppingCart" do
   describe :item_for do
     context "the item is in the cart" do
       before(:each) do
-        @product = Product.create
-        @cart.add(@product, 1)
+        @some_object = SomeClass.create
+        @cart.add(@some_object, 1)
       end
 
       it "returns the item object" do
-        @cart.item_for(@product).item.should == @product
+        @cart.item_for(@some_object).item.should == @some_object
       end
     end
 
     context "the item is not on the cart" do
       it "returns nil" do
-        product = Product.create
+        product = SomeClass.create
         @cart.item_for(product).should be_nil
       end
     end
@@ -63,17 +63,17 @@ describe "ActsAsShoppingCart" do
 
   describe :add do
     it "adds an item" do
-      @product = Product.create
-      @cart.add(@product, 100)
+      @some_object = SomeClass.create
+      @cart.add(@some_object, 100)
       @cart.cart_items(true).first.should_not be_nil
-      @cart.cart_items(true).first.item.should == @product
+      @cart.cart_items(true).first.item.should == @some_object
     end
 
     context "add more of an item already in the cart" do
       it "increases the quantity of the item" do
-        @product = Product.create
-        @cart.add(@product, 100)
-        @cart.add(@product, 100, 2)
+        @some_object = SomeClass.create
+        @cart.add(@some_object, 100)
+        @cart.add(@some_object, 100, 2)
 
         @cart.cart_items.first.quantity.should == 3
       end
@@ -87,8 +87,8 @@ describe "ActsAsShoppingCart" do
 
     context "the cart has items" do
       before(:each) do
-        @cart.add(Product.create, 199.99, 2)
-        @cart.add(Product.create, 299.99)
+        @cart.add(SomeClass.create, 199.99, 2)
+        @cart.add(SomeClass.create, 299.99)
       end
 
       it "should return the sum of the item prices" do
@@ -106,47 +106,47 @@ describe "ActsAsShoppingCart" do
   describe :remove do
     context "the cart has items" do
       before(:each) do
-        @product = Product.create
-        @cart.add(@product, 199.99)
-        @cart.add(Product.create, 299.99)
+        @some_object = SomeClass.create
+        @cart.add(@some_object, 199.99)
+        @cart.add(SomeClass.create, 299.99)
       end
 
       it "removes the item from the cart" do
-        @cart.remove(@product)
+        @cart.remove(@some_object)
         @cart.cart_items.count.should == 1
-        @cart.item_for(@product).should be_nil
+        @cart.item_for(@some_object).should be_nil
       end
     end
 
     context "remove some items" do
       before(:each) do
-        @product = Product.create
-        @cart.add(@product, 199.99, 5)
-        @cart.add(Product.create, 299.99)
+        @some_object = SomeClass.create
+        @cart.add(@some_object, 199.99, 5)
+        @cart.add(SomeClass.create, 299.99)
       end
 
       it "removes 2 items of the specific product" do
-        @cart.remove(@product, 2)
-        @cart.item_for(@product).should_not be_nil
-        @cart.item_for(@product).quantity.should == 3
+        @cart.remove(@some_object, 2)
+        @cart.item_for(@some_object).should_not be_nil
+        @cart.item_for(@some_object).quantity.should == 3
       end
     end
 
     context "the object is not on the cart" do
       before(:each) do
-        @product = Product.create
+        @some_object = SomeClass.create
       end
       it "does nothing" do
-        @cart.remove(@product)
+        @cart.remove(@some_object)
       end
     end
 
     describe :total_unique_items do
       context "there are different items in the cart" do
         before(:each) do
-          @cart.add(Product.create, 100, 1)
-          @cart.add(Product.create, 100, 2)
-          @cart.add(Product.create, 100, 3)
+          @cart.add(SomeClass.create, 100, 1)
+          @cart.add(SomeClass.create, 100, 2)
+          @cart.add(SomeClass.create, 100, 3)
         end
 
         it "returns the sum of all the item quantities" do
@@ -164,19 +164,19 @@ describe "ActsAsShoppingCart" do
     describe :subtotal_for do
       context "the item exists in the cart" do
         before(:each) do
-          @product = Product.create
-          @cart.add(@product, 300, 5)
-          @cart.add(Product.create, 100, 3)
+          @some_object = SomeClass.create
+          @cart.add(@some_object, 300, 5)
+          @cart.add(SomeClass.create, 100, 3)
         end
 
         it "returns the quantity times the price for the specicfied object" do
-          @cart.subtotal_for(@product).should == (300 * 5)
+          @cart.subtotal_for(@some_object).should == (300 * 5)
         end
       end
 
       context "the item doesn't exist on the cart" do
         it "returns nil" do
-          @cart.subtotal_for(Product.create).should be_nil
+          @cart.subtotal_for(SomeClass.create).should be_nil
         end
       end
     end
