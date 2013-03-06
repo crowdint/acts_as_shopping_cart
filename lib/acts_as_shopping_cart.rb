e041d7091c5a8ce175d3619dcc682b8c3ccca38c
David Padilla <david@crowdint.com>
2013-03-05 20:16:12 -0600

    Remove dead code

--- lib/acts_as_shopping_cart.rb
+++ lib/acts_as_shopping_cart.rb
@@ -1,13 +1,6 @@
 require 'acts_as_shopping_cart/version'
 require 'active_record/acts/shopping_cart'
 require 'active_record/acts/shopping_cart_item'
-
-# require 'active_record/acts/shopping_cart/cart_instance_methods'
-# require 'active_record/acts/shopping_cart/item_instance_methods'
-
-# require 'active_record/acts/shopping_cart_item'
-# require 'active_record/acts/shopping_cart_item/cart_item_instance_methods'
-
 require 'acts_as_shopping_cart/schema'
 
 module ActiveRecord
